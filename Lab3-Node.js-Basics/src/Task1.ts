a7f7cc3d81c75b0d30013d96c2459b637a07b215
xd6d <Kurkin566@gmail.com>
2023-05-22 00:44:01 +0300

    Task1.ts refactor

--- Lab3-Node.js-Basics/src/Task1.ts
+++ Lab3-Node.js-Basics/src/Task1.ts
@@ -1,8 +1,8 @@
 export {}
 const array: Array<string> = ["one", "two", "three"];
 
-async function runSequent<T>(array: Array<T>, fn: (item: T, index: number) => Promise<Awaited<{ item: T; index: number }>>) {
-    const res = []
+async function runSequent<T>(array: Array<T>, fn: (item: T, index: number) => Promise<{ item: T; index: number }>) {
+    const res: { item: T; index: number }[] = []
     for (let i = 0; i<array.length; i++){
         res.push(await fn(array[i], i))
     }
