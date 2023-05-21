88552078266b6bf22456f1f08c97dfea1bc5a1c5
xd6d <Kurkin566@gmail.com>
2023-05-22 00:38:26 +0300

    code simplification

--- Lab3-Node.js-Basics/src/Task2.ts
+++ Lab3-Node.js-Basics/src/Task2.ts
@@ -2,11 +2,8 @@ const array = [1, 2, 3, 6, 7, 9];
 
 function arrayChangeDelete<T>(array: T[], toDelete: (item: T) => boolean) {
     let result = array.filter(toDelete)
-    let index = array.findIndex(toDelete)
-    while (index != -1) {
-        array.splice(index, 1)
-        index = array.findIndex(toDelete)
-    }
+    while (array.findIndex(toDelete) != -1)
+        array.splice(array.findIndex(toDelete), 1)
     return result
 }
 
