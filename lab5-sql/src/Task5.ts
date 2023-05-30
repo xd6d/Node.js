98b7040675d8db86608ca13018a3f40d52ac5de4
xd6d <Kurkin566@gmail.com>
2023-05-30 15:33:40 +0300

    added parameter for Task5.ts

--- lab5-sql/src/Task5.ts
+++ lab5-sql/src/Task5.ts
@@ -1,16 +1,16 @@
 import {db} from "./database.js";
 
-function execute() {
+function execute(date: Date) {
     return db.result(
         "select videos.* from videos " +
         "inner join likes on (likes.video_id = videos.id) " +
-        "where videos.published_at > timestamp '2021-09-01 00:00:00'" +
+        `where videos.published_at > timestamp '${date.toISOString().slice(0, 10)} 00:00:00'` +
         "group by videos.id " +
         "having count(*) > 4 " +
         "order by count(*) desc " +
         "limit 10")
 }
 
-execute()
+execute(new Date(2021, 8))
     .then(result => console.log(result.rows))
     .catch(err => console.log(err))
--- lab5-sql/src/database.ts
+++ lab5-sql/src/database.ts
@@ -4,4 +4,3 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const db = pgp()(`postgres://${process.env.postgres_username}:${process.env.postgres_password}@${process.env.postgres_host}:${process.env.postgres_port}/${process.env.postgres_database}`)
-
